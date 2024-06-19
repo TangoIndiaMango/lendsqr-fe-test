bcabcbf767ff1bf1f7624ee31f82eed4cd8927b6
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2024-06-19 20:05:37 +0100

    wip:update env

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,4 +1,3 @@
-// const apiUrl = import.meta.env.VITE_API_URL
-const apiUrl = 'https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1'
+const apiUrl = import.meta.env.VITE_API_URL
 
 export const usersUrl = `${apiUrl}/users`
